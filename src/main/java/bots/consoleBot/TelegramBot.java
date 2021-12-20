2ae201ab999d9e1efd976f753f580aad92890bf1
korsham <butaluk1337@mail.ru>
2021-12-21 02:01:04 +0500

    Добавлен UI(кнопки) и произведен рефакторинг кода.

--- src/main/java/bots/consoleBot/TelegramBot.java
+++ src/main/java/bots/consoleBot/TelegramBot.java
@@ -5,8 +5,13 @@ import dataIO.telegramInput.TelegramInput;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import tokenizer.Tokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TelegramBot extends TelegramLongPollingBot {
     private final Tokenizer tokenizer;
@@ -14,14 +19,16 @@ public class TelegramBot extends TelegramLongPollingBot {
     private final String botUsername;
     private final String botToken;
     private final TelegramInput telegramInput;
+    private final ReplyKeyboardMarkup replykeyboardMarkup;
     private Update message;
 
     public TelegramBot(String botUsername, String botToken) {
         this.botUsername = botUsername;
         this.botToken = botToken;
         telegramInput = new TelegramInput();
-        this.tokenizer = new Tokenizer(telegramInput);
-        this.commandHandler = new CommandHandler();
+        tokenizer = new Tokenizer(telegramInput);
+        commandHandler = new CommandHandler();
+        replykeyboardMarkup = new ReplyKeyboardMarkup();
     }
 
     public void onUpdateReceived(Update update) {
@@ -35,29 +42,74 @@ public class TelegramBot extends TelegramLongPollingBot {
             telegramInput.message = userMessage.getMessage().getText();
             var token = tokenizer.getNextToken();
             var message = commandHandler.handleCommand(token);
-            SendMessage sendMessage = new SendMessage();
-            sendMessage.setChatId(userMessage.getMessage().getChatId().toString());
-            sendMessage.setText(message.message());
-            try {
-                execute(sendMessage);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+            initializeKeyboard();
+            inputMessage(initializeSendMessage(userMessage.getMessage().getChatId().toString(), message.message()));
         }
     }
 
+    @Override
+    public String getBotUsername() { return botUsername; }
 
     @Override
-    public String getBotUsername() {
-        return botUsername;
+    public String getBotToken() { return botToken; }
+
+    public void inputMessage(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public String getBotToken() {
-        return botToken;
+    private Update outputMessage() { return message; }
+
+    private void initializeKeyboard()
+    {
+        var keyboard = new ArrayList<KeyboardRow>();
+        var firstKeyboardRow = new KeyboardRow();
+        var secondKeyboardRow = new KeyboardRow();
+        var thirdKeyboardRow = new KeyboardRow();
+
+        addKeyboardButtonsInARow(
+                firstKeyboardRow,
+                new KeyboardButton("/advise"),
+                new KeyboardButton("/rating"),
+                new KeyboardButton("/year"));
+
+        addKeyboardButtonsInARow(
+                secondKeyboardRow,
+                new KeyboardButton("/year"),
+                new KeyboardButton("/type"),
+                new KeyboardButton("/country"));
+
+        thirdKeyboardRow.add(new KeyboardButton("/help"));
+        addRowsInKeyboard(keyboard, firstKeyboardRow, secondKeyboardRow, thirdKeyboardRow);
+        initializeReplyKeyboardMarkup(keyboard);
     }
 
-    public Update outputMessage() {
-        return message;
+    private void initializeReplyKeyboardMarkup(ArrayList<KeyboardRow> keyboard)
+    {
+        replykeyboardMarkup.setSelective(true);
+        replykeyboardMarkup.setResizeKeyboard(true);
+        replykeyboardMarkup.setOneTimeKeyboard(false);
+        replykeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    private void addKeyboardButtonsInARow(KeyboardRow keyboardRow, KeyboardButton ... buttons)
+    {
+        Collections.addAll(keyboardRow, buttons);
+    }
+
+    private void addRowsInKeyboard(ArrayList<KeyboardRow> keyboard, KeyboardRow ... rows) {
+        Collections.addAll(keyboard, rows);
+    }
+
+    private SendMessage initializeSendMessage(String chatId, String message)
+    {
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(message);
+        sendMessage.setReplyMarkup(replykeyboardMarkup);
+        return sendMessage;
     }
 }
